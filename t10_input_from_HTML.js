2b3fa4547e006d704de3e97e2caa80934ba51b8b
24077fs <24077fs@hvhs.school.nz>
2026-05-07 13:28:51 +1200

    Update

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,1 +1,64 @@
-/* Paste your code from the last task */
+/****************************
+Name of task:t09_activate_via_button
+****************************/
+console.log("Running t09_activate_via_button.js")
+
+//Variables
+var user = "Finn"
+var pocketmoney = 4546;
+var age = 15;
+var yearborn = 2011;
+
+
+
+/**************************** 
+Main Code
+
+
+****************************/
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<p>Added by JavaScript</p>";
+OUTPUT.innerHTML += "<p>Hello " + user + "</p>";
+OUTPUT.innerHTML += "<p>Second Paragraph line.</p>";
+OUTPUT.innerHTML += "<p>Third Paragraph line.</p>";
+
+
+
+
+
+
+/****************************
+Functions
+****************************/
+
+function start() {
+console.log("Hi " + user)
+console.log("As of "+"today "+"you are "+age+" years old.")
+console.log("You were born in "+yearborn)
+console.log("In 10 years you will be "+(age+10)+" years old")
+console.log("You have "+pocketmoney+ " dollars")
+console.log("You spend half of your money, now you have "+(pocketmoney/2))
+pocketmoney = pocketmoney/2;
+console.log("Then you get $3, now you have "+(pocketmoney+3))
+
+}
+
+function getFormInput(){
+  const NAME_FIELD = document.getElementById("nameField");
+  let userName = NAME_FIELD.value;
+  OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
+}
+
+
+function welcome(){
+
+OUTPUT.innerHTML += "<p>Less code matters</p>"
+}
+/****************************
+Comment
+****************************/
+const NAME_FIELD = document.getElementById("nameField");
+let userName = NAME_FIELD.value;
+
+welcome()
+welcome()
