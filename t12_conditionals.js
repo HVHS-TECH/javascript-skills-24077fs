c1f6569352c03abae0eab7107c2055a0eb1c1b38
24077fs <24077fs@hvhs.school.nz>
2026-05-26 09:04:27 +1200

    Update

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -4,10 +4,7 @@ Name of task:t11_data_types
 console.log("Running t11_data_types.js")
 
 //Variables
-var user = "Finn"
-var pocketmoney = 4546;
-var age = 15;
-var yearborn = 2011;
+
 
 
 
@@ -17,11 +14,10 @@ Main Code
 
 ****************************/
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-const OUTPUT1 = document.getElementById("spaceForJavaScriptOutputMoney");
-OUTPUT.innerHTML = "<p>Added by JavaScript</p>";
-OUTPUT.innerHTML += "<p>Hello " + user + "</p>";
-OUTPUT.innerHTML += "<p>Second Paragraph line.</p>";
-OUTPUT.innerHTML += "<p>Third Paragraph line.</p>";
+
+
+
+
 
 
 
@@ -30,11 +26,11 @@ OUTPUT.innerHTML += "<p>Third Paragraph line.</p>";
 
 /****************************
 Functions
-****************************/
+
 
 function start() {
-console.log("Hi " + user)
-console.log("As of "+"today "+"you are "+age+" years old.")
+console.log("Hi " + userName)
+console.log("As of "+"today "+"you are "+Age+" years old.")
 console.log("You were born in "+yearborn)
 console.log("In 10 years you will be "+(age+10)+" years old")
 console.log("You have "+pocketmoney+ " dollars")
@@ -43,32 +39,32 @@ pocketmoney = pocketmoney/2;
 console.log("Then you get $3, now you have "+(pocketmoney+3))
 
 }
-
+****************************/
 function getFormInput(){
   const NAME_FIELD = document.getElementById("nameField");
-  let userName = NAME_FIELD.value;
-  OUTPUT.innerHTML = "<p>Your name is "+userName+"</p>";
+  var userName = NAME_FIELD.value;
+
+
+  const AGE_FIELD = document.getElementById("ageField");
+  var Age = AGE_FIELD.value;
+  OUTPUT.innerHTML += "<p>You are  "+Age+" years old</p>";
+  
+
+  const MONEY_FIELD = document.getElementById("moneyField");
+  var pocketMoney = MONEY_FIELD.value;
+  OUTPUT.innerHTML += "<p>You have $"+pocketMoney+" dollars.</p>";
+  if(pocketMoney>=4){ 
+    OUTPUT.innerHTML += "<p>A choclate bar costs 4$<br> You can afford a chocolate bar<p>"
+  } else{
+  OUTPUT.innerHTML += "<p>A choclate bar costs 4$<br> You can't afford a chocolate bar<p>"
+  }  
 }
 
 
-function getFormInputMoney(){
-  const NAME_FIELD = document.getElementById("moneyField");
-  let pocketMoney = MONEY_FIELD.value;
-  OUTPUT1.innerHTML = "<p>You have $"+pocketMoney+" dollars.</p>";
-}
 
-function welcome(){
-
-OUTPUT.innerHTML += "<p>Less code matters</p>"
-}
 /****************************
 Comment
 ****************************/
-const NAME_FIELD = document.getElementById("nameField");
-let userName = NAME_FIELD.value;
 
-const MONEY_FIELD = document.getElementById("moneyField");
-let pocketMoney = MONEY_FIELD.value;
 
-welcome()
-welcome()
+
--- t13_functions_return.js
+++ t13_functions_return.js
@@ -1,1 +1,81 @@
-/* Paste your code from the last task */
+/****************************
+Name of task:t11_data_types
+****************************/
+console.log("Running t13_functions_return.js")
+
+//Variables
+
+
+
+
+/**************************** 
+Main Code
+
+
+****************************/
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+/****************************
+Functions
+
+
+function start() {
+console.log("Hi " + userName)
+console.log("As of "+"today "+"you are "+Age+" years old.")
+console.log("You were born in "+yearborn)
+console.log("In 10 years you will be "+(age+10)+" years old")
+console.log("You have "+pocketmoney+ " dollars")
+console.log("You spend half of your money, now you have "+(pocketmoney/2))
+pocketmoney = pocketmoney/2;
+console.log("Then you get $3, now you have "+(pocketmoney+3))
+
+}
+****************************/
+
+
+function getFormInput(){
+  const NAME_FIELD = document.getElementById("nameField");
+  var userName = NAME_FIELD.value;
+
+
+  const AGE_FIELD = document.getElementById("ageField");
+  var Age = AGE_FIELD.value;
+  OUTPUT.innerHTML += "<p>You are  "+Age+" years old</p>";
+  
+     
+  const MONEY_FIELD = document.getElementById("moneyField");
+  var pocketMoney = MONEY_FIELD.value;
+  OUTPUT.innerHTML += "<p>You have $"+pocketMoney+" dollars.</p>";
+  if(pocketMoney>=4){ 
+    OUTPUT.innerHTML += "<p>A choclate bar costs 4$<br> You can afford a chocolate bar<p>"
+  } else{
+  OUTPUT.innerHTML += "<p>A choclate bar costs 4$<br> You can't afford a chocolate bar<p>"
+  }  
+}
+
+function calculatePay(_hours){
+
+var pay = _hours * 18.80;
+pay = pay - (pay * 0.105);
+pay = pay - (pay * 0.04);
+pay = pay + 10;
+return pay;
+
+}
+
+
+
+var mondayPay = calculatePay(8);
+var tuesdayPay = calculatepPay(6);
+
+OUTPUT.innerHTML += "This Weeks pay:<br>"
+OUTPUT.innerHTML += "Monday: $"+mondayPay+"<br>"
+OUTPUT.innerHTML += "Monday: $"+tuesdayPay+"<br>"
+
+
+/****************************
+Comment
+****************************/
+
+
+
